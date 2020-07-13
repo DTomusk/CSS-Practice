22af100ca58121882864d47493aa690afdf3ec3a
DTomusk <david.tomusk@gmail.com>
2020-07-13 08:10:35 +0100

    Played around with dropdown menus and navbars

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get('/flex', function(req, res) {
 	res.render('flex', {})
 })
 
+app.get('/drop', function(req, res) {
+	res.render('drop', {})
+})
+
+app.get('/boxes', function(req, res) {
+	res.render('boxes', {})
+})
+
 app.listen(3000, function() {
 	console.log("Listening on 3000");
 });
